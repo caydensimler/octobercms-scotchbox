f75e314e3537e20b86982c38661fae0fb57d16dc
Cayden <cayden.simler@hotmail.com>
2017-09-18 11:56:48 -0500

    Added price and contact email to posts table.

---
+++ public/plugins/caydensimler/craigslist/updates/builder_table_update_caydensimler_craigslist_categories.php
@@ -0,0 +1,23 @@
+<?php namespace CaydenSimler\Craigslist\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateCaydensimlerCraigslistCategories extends Migration
+{
+    public function up()
+    {
+        Schema::table('caydensimler_craigslist_categories', function($table)
+        {
+            $table->increments('id')->unsigned(false)->change();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('caydensimler_craigslist_categories', function($table)
+        {
+            $table->increments('id')->unsigned()->change();
+        });
+    }
+}
---
+++ public/plugins/caydensimler/craigslist/updates/builder_table_update_caydensimler_craigslist_post_3.php
@@ -0,0 +1,27 @@
+<?php namespace CaydenSimler\Craigslist\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateCaydensimlerCraigslistPost3 extends Migration
+{
+    public function up()
+    {
+        Schema::table('caydensimler_craigslist_post', function($table)
+        {
+            $table->integer('price')->nullable()->unsigned();
+            $table->string('contact_email', 20);
+            $table->text('body')->nullable()->change();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('caydensimler_craigslist_post', function($table)
+        {
+            $table->dropColumn('price');
+            $table->dropColumn('contact_email');
+            $table->text('body')->nullable(false)->change();
+        });
+    }
+}
