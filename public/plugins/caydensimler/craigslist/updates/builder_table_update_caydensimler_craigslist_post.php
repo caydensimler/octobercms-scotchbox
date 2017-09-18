e8dff641a50cfad5ef804c3384a11f51f27d448b
Cayden <cayden.simler@hotmail.com>
2017-09-18 11:49:21 -0500

    Updated the posts table to include timestamps, as well as soft deletion support.

---
+++ public/plugins/caydensimler/craigslist/updates/builder_table_update_caydensimler_craigslist_post.php
@@ -0,0 +1,25 @@
+<?php namespace CaydenSimler\Craigslist\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateCaydensimlerCraigslistPost extends Migration
+{
+    public function up()
+    {
+        Schema::table('caydensimler_craigslist_post', function($table)
+        {
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('caydensimler_craigslist_post', function($table)
+        {
+            $table->dropColumn('created_at');
+            $table->dropColumn('updated_at');
+        });
+    }
+}
---
+++ public/plugins/caydensimler/craigslist/updates/builder_table_update_caydensimler_craigslist_post_2.php
@@ -0,0 +1,23 @@
+<?php namespace CaydenSimler\Craigslist\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateCaydensimlerCraigslistPost2 extends Migration
+{
+    public function up()
+    {
+        Schema::table('caydensimler_craigslist_post', function($table)
+        {
+            $table->timestamp('deleted_at')->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('caydensimler_craigslist_post', function($table)
+        {
+            $table->dropColumn('deleted_at');
+        });
+    }
+}
