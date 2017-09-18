66d2de1a47817119ecb746edb8928dfd780182fe
Cayden <cayden.simler@hotmail.com>
2017-09-18 11:11:42 -0500

    Added tables for categories, subcategories, and user posts. Still have to create the pivot tables to link everything.

---
+++ public/plugins/caydensimler/craigslist/updates/builder_table_create_caydensimler_craigslist_post.php
@@ -0,0 +1,25 @@
+<?php namespace CaydenSimler\Craigslist\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableCreateCaydensimlerCraigslistPost extends Migration
+{
+    public function up()
+    {
+        Schema::create('caydensimler_craigslist_post', function($table)
+        {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('title', 50);
+            $table->text('body');
+            $table->integer('subcategory_id');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::dropIfExists('caydensimler_craigslist_post');
+    }
+}
---
+++ public/plugins/caydensimler/craigslist/updates/builder_table_create_caydensimler_craigslist_subcategories.php
@@ -0,0 +1,23 @@
+<?php namespace CaydenSimler\Craigslist\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableCreateCaydensimlerCraigslistSubcategories extends Migration
+{
+    public function up()
+    {
+        Schema::create('caydensimler_craigslist_subcategories', function($table)
+        {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->integer('category_id');
+            $table->string('subcategory');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::dropIfExists('caydensimler_craigslist_subcategories');
+    }
+}
