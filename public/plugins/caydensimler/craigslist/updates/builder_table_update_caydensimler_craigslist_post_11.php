e0bd6791d66530950394c0e8d8a0edbdd43cae81
Cayden <cayden.simler@hotmail.com>
2017-09-22 11:50:46 -0500

    Removed the updated_at column in the post table.

---
+++ public/plugins/caydensimler/craigslist/updates/builder_table_update_caydensimler_craigslist_post_11.php
@@ -0,0 +1,23 @@
+<?php namespace CaydenSimler\Craigslist\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateCaydensimlerCraigslistPost11 extends Migration
+{
+    public function up()
+    {
+        Schema::table('caydensimler_craigslist_post', function($table)
+        {
+            $table->dropColumn('updated_at');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('caydensimler_craigslist_post', function($table)
+        {
+            $table->timestamp('updated_at')->nullable();
+        });
+    }
+}
